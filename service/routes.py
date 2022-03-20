d1f7d0e6a4c0be8de3e772616a7dffafac1afb03
Namrata Khanijow <nk3212@nyu.edu>
2022-03-20 20:26:19 +0000

    added delete file

--- service/routes.py
+++ service/routes.py
@@ -98,6 +98,23 @@ def update_shopcarts(customer_id, product_id):
     return make_response(jsonify(shopcart.serialize()), status.HTTP_200_OK)
 
 ######################################################################
+# DELETE A PET
+######################################################################
+@app.route("/shopcarts/<int:shopcart_id>", methods=["DELETE"])
+def delete_shopcarts(shopcart_id):
+    """
+    Delete a Shopcart
+    This endpoint will delete a Shopcart based the id specified in the path
+    """
+    app.logger.info("Request to delete shopcart with id: %s", shopcart_id)
+    shopcart = Shopcart.find(shopcart_id)
+    if shopcart:
+        shopcart.delete()
+
+    app.logger.info("Shopcart with ID [%s] delete complete.", shopcart_id)
+    return make_response("", status.HTTP_204_NO_CONTENT)
+    
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
